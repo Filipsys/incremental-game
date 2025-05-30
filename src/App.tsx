8513503619de1d290f4ce7b7b02cfc1055294745
Filipsys <filipsysak@gmail.com>
2025-05-30 23:57:31 +0200

    Basic transactions increase based on rate

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,32 @@ import { create } from "zustand";
 
 type GameStore = {
   ticks: number;
+  transactions: number;
+  transactionsPerTick: number;
   setTicks: (ticks: number) => void;
+  setTransactionsPerTick: (transactionsPerTick: number) => void;
 
   startTick: () => void;
 };
 
 const useStore = create<GameStore>((set) => ({
   ticks: 0,
+  transactions: 0,
+  transactionsPerTick: 0.1,
   setTicks: (ticks: number) => set({ ticks: ticks }),
+  setTransactionsPerTick: (transactionsPerTick: number) =>
+    set({ transactionsPerTick: transactionsPerTick }),
 
-  startTick: () => set((state) => ({ ticks: state.ticks + 1 })),
+  startTick: () =>
+    set((state) => ({
+      ticks: state.ticks + 1,
+      transactions: state.transactions + state.transactionsPerTick,
+    })),
 }));
 
 function App() {
   const ticks = useStore((state) => state.ticks);
+  const transactions = useStore((state) => state.transactions);
   const startTick = useStore((state) => state.startTick);
 
   useEffect(() => {
@@ -27,7 +39,8 @@ function App() {
 
   return (
     <>
-      <p>{ticks}</p>
+      <p>Ticks: {ticks}</p>
+      <p>Transactions: {transactions}</p>
     </>
   );
 }
