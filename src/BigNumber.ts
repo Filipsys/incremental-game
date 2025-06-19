42b105eefd0bcd69bbf2ebd9d3fec4d29d56a3d1
Filipsys <filipsysak@gmail.com>
2025-06-19 23:50:31 +0200

    Added methods for logical operator checks to the BigNumber class

--- src/BigNumber.ts
+++ src/BigNumber.ts
@@ -1,5 +1,9 @@
 import type { customBigNumber } from "./types/main";
 
+// Methods needed:
+// toLiteral()
+// plus()
+// minus()
 export class BigNumber {
   base: customBigNumber["base"];
   exponent: customBigNumber["exponent"];
@@ -21,5 +25,39 @@ export class BigNumber {
       this.base.mul(number.base),
       this.exponent + number.exponent,
     );
+  }
+
+  lessThan(number: BigNumber): boolean {
+    if (this.base.lessThan(number.base) && this.exponent < number.exponent)
+      return true;
+
+    return false;
+  }
+
+  lessThanOrEqualTo(number: BigNumber): boolean {
+    if (
+      this.base.lessThanOrEqualTo(number.base) &&
+      this.exponent <= number.exponent
+    )
+      return true;
+
+    return false;
+  }
+
+  greaterThan(number: BigNumber): boolean {
+    if (this.base.greaterThan(number.base) && this.exponent > number.exponent)
+      return true;
+
+    return false;
+  }
+
+  greaterThanOrEqualTo(number: BigNumber): boolean {
+    if (
+      this.base.greaterThanOrEqualTo(number.base) &&
+      this.exponent >= number.exponent
+    )
+      return true;
+
+    return false;
   }
 }
