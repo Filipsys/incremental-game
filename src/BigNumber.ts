9b712648a817b932a3a4a1a5f026fc225279c0ab
Filipsys <filipsysak@gmail.com>
2025-06-20 23:50:32 +0200

    Fixed Decimal type, added add method & fixed logic methods

--- src/BigNumber.ts
+++ src/BigNumber.ts
@@ -1,18 +1,16 @@
+import { Decimal } from "decimal.js";
+
 import type { customBigNumber } from "./types/main";
 
 // Methods needed:
 // toLiteral()
-// plus()
 // minus()
 export class BigNumber {
   base: customBigNumber["base"];
   exponent: customBigNumber["exponent"];
 
-  constructor(
-    base: customBigNumber["base"],
-    exponent: customBigNumber["exponent"],
-  ) {
-    this.base = base;
+  constructor(base: Decimal.Value, exponent: customBigNumber["exponent"]) {
+    this.base = new Decimal(base);
     this.exponent = exponent;
   }
 
@@ -27,8 +25,32 @@ export class BigNumber {
     );
   }
 
+  add(number: BigNumber): BigNumber {
+    if (this.exponent === number.exponent) {
+      return new BigNumber(this.base.add(number.base), this.exponent);
+    }
+
+    const exponentDifference = this.exponent - number.exponent;
+
+    if (exponentDifference > 0n) {
+      const scale = new Decimal(10).pow(exponentDifference.toString());
+      const newBase = this.base.add(number.base.div(scale));
+
+      return new BigNumber(newBase, this.exponent);
+    } else {
+      const scale = new Decimal(10).pow(-exponentDifference.toString());
+      const newBase = this.base.div(scale).add(number.base);
+
+      return new BigNumber(newBase, this.exponent);
+    }
+  }
+
   lessThan(number: BigNumber): boolean {
-    if (this.base.lessThan(number.base) && this.exponent < number.exponent)
+    if (
+      (this.base.lessThanOrEqualTo(number.base) &&
+        this.exponent < number.exponent) ||
+      (this.base.lessThan(number.base) && this.exponent <= number.exponent)
+    )
       return true;
 
     return false;
@@ -45,7 +67,11 @@ export class BigNumber {
   }
 
   greaterThan(number: BigNumber): boolean {
-    if (this.base.greaterThan(number.base) && this.exponent > number.exponent)
+    if (
+      (this.base.greaterThanOrEqualTo(number.base) &&
+        this.exponent > number.exponent) ||
+      (this.base.greaterThan(number.base) && this.exponent >= number.exponent)
+    )
       return true;
 
     return false;
